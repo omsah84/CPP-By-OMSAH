8d8c8dc2ce76e430bd77cb1775d2e58fa066d25e
omsah <huncyoms@gmail.com>
2023-06-04 14:32:45 -0400

    commit

---
+++ Day4-Practice/Bitwise-and.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+void bitwiseand(int a,int b){
+
+}
+
+void bitwiseor(int a, int b){
+
+}
+
+void bitwisenot(int a){
+    
+}
+
+void bitwisexor(int a, int b){
+
+}
+
+void bitwiseleftshift(int a, int pos){
+
+}
+
+void bitwiserightshift(int a, int pos){
+    
+}
+
+
+int main(){
+    int a, b;
+
+    cout << "Enter the number is: ";
+    cout << endl;
+    cin >> a >> b;
+
+    bitwiseand(a, b);
+    bitwiseor(a, b);
+    bitwisenot(a);
+    bitwisexor(a, b);
+    bitwiseleftshift(a,1);
+    bitwiserightshift(b, 1);
+
+    return 0;
+}
