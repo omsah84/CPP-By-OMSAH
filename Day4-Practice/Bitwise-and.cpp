f1585c5577e3e5dd63cc7c95188406f6aa224bae
omsah <huncyoms@gmail.com>
2023-06-04 14:38:39 -0400

    commit

--- Day4-Practice/Bitwise-and.cpp
+++ Day4-Practice/Bitwise-and.cpp
@@ -2,26 +2,36 @@
 using namespace std;
 void bitwiseand(int a,int b){
 
+    cout << "The bitwise and operation: " << (a & b);
+    cout << endl;
 }
 
 void bitwiseor(int a, int b){
-
+    cout << "The bitwise or operation: " << (a | b);
+    cout << endl;
 }
 
 void bitwisenot(int a){
-    
+    cout << "The bitwise not operation: " << (~a);
+    cout << endl;
 }
 
 void bitwisexor(int a, int b){
 
+    cout << "The bitwise xor operation: " << (a ^ b);
+    cout << endl;
 }
 
 void bitwiseleftshift(int a, int pos){
 
+    cout << "The bitwise left shift operation: " << (a << pos);
+    cout << endl;
 }
 
 void bitwiserightshift(int a, int pos){
-    
+
+    cout << "The bitwise right shift operation: " << (a >> pos);
+    cout << endl;
 }
 
 
