4a3ea8683d03e131466daec85d6e429066641730
omsah <huncyoms@gmail.com>
2023-06-04 15:00:28 -0400

    variable

---
+++ Day4-Practice/variable-in-cpp.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    float b;
+    double c;
+    bool d;
+    long e;
+
+    cout << sizeof(a);
+    cout << endl;
+    cout << sizeof(b);
+    cout << endl;
+    cout << sizeof(c);
+    cout << endl;
+    cout << sizeof(d);
+    cout << endl;
+    cout << sizeof(e);
+
+}
