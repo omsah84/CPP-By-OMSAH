a285acbbbfb8e24948a5c6bd3bb28456da6bccca
omsah <huncyoms@gmail.com>
2023-06-04 11:48:01 -0400

    print odd

--- Day2-Practice/EvenNum.cpp
+++ Day2-Practice/EvenNum.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
+void printOdd(int num){
+    for (int i = 0; i <= num;i++){
+        if(i%2 !=0){
+            cout << "The odd number is: " << i;
+            cout << endl;
+        }
+    }
+}
 
 void printEven(int num){
     for (int i = 0; i <= num;i++){
@@ -18,6 +26,8 @@ int main(){
     int n;
 
     cin >> n;
-    printEven(n);
+    // printEven(n);
+    printOdd(n);
+    
     return 0;
 }
