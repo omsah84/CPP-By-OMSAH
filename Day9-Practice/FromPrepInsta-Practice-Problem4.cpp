be32efc305b74a243e722cfb4037f87f07b33c5a
OM SAH <huncyoms@gmail.com>
2023-09-08 11:59:17 -0400

    commit

--- Day9-Practice/FromPrepInsta-Practice-Problem4.cpp
+++ Day9-Practice/FromPrepInsta-Practice-Problem4.cpp
@@ -1,2 +1,18 @@
 //number is even or odd in c/c++
 #include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter the number: ";
+    cin>>number;
+
+    if(number % 2 == 0){
+        cout<<"The entered number is even..";
+    }else{
+        cout<<"The entered number is odd..";
+    }
+    
+    return 0;
+    
+}
