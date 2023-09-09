791677ac3c64a0135f18269ef59cba749ce0c5ea
OM SAH <huncyoms@gmail.com>
2023-09-09 13:57:50 -0400

    commit

--- Day11-Practice/FromPrepInsta-Practice-Problem10.cpp
+++ Day11-Practice/FromPrepInsta-Practice-Problem10.cpp
@@ -1,38 +1,45 @@
-//HCF/GCD of two number
-#include<iostream>
+// HCF/GCD of two number
+#include <iostream>
 using namespace std;
 
-//hcf function
-int hcf(int num1, int num2){
-    int number=num1;
-    if(num1>num2){
+// hcf function
+int hcf(int num1, int num2)
+{
+    int number = num1;
+    if (num1 > num2)
+    {
         number = num2;
-    }else{
+    }
+    else
+    {
         number = num1;
     }
 
-    int hc= 1;
-    for(int i = 1 ;i<number ;i++){
-        if(num1%i==0 && num2%i==0){
-            hc= i;
+    int hc = 1;
+    for (int i = 1; i < number; i++)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
+            hc = i;
         }
     }
     return hc;
 }
 
-//drive code
-int main(){
+// drive code
+int main()
+{
     int number1, number2;
 
-    //input from user
-    cout<<"Enter a first number: ";
-    cin>>number1;
+    // input from user
+    cout << "Enter a first number: ";
+    cin >> number1;
 
-    cout<<"Enter a second number: ";
-    cin>>number2;
+    cout << "Enter a second number: ";
+    cin >> number2;
 
     int ans = hcf(number1, number2);
-    cout<<"The HCF number is "<<ans;
-    
+    cout << "The HCF number is " << ans;
+
     return 0;
 }
