1a0dcc598a3f07f1e3a05fa28c47dd8fb915d5c7
OM SAH <huncyoms@gmail.com>
2023-09-25 01:13:24 -0400

    commit

---
+++ Day12-Practice/FromPrepInsta-Practice-Problem4.cpp
@@ -0,0 +1,32 @@
+//check if leap year of not
+
+#include<iostream>
+using namespace std;
+
+bool isLeapYear(int year){
+
+    if((year%4 == 0 && year%100 == 0) || year%400 == 0){
+        return true;
+    }
+
+    return false;
+    
+}
+
+int main(){
+    // year%4==0 && year%100 !=0 || year%400==0 
+
+    int year;
+    cout<<"Enter year: ";
+    cin>>year;
+
+    if(isLeapYear(year)){
+        cout<<"Yes given year is a leap year..";
+    }
+    else{
+        cout<<"Not given year is not a leap year..";
+    }
+
+    
+    return 0;
+}
