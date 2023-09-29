f0ec056c5c295d933a2b5dfe77157fb2c8678009
OM SAH <huncyoms@gmail.com>
2023-09-29 11:13:07 -0400

    commit

--- Day13-Practice/Creating-Node.cpp
+++ Day13-Practice/Creating-Node.cpp
@@ -38,6 +38,38 @@ void insertAtHead(Node *&head, int data)
     head = newNode;
 }
 
+
+void insertAtEnd(Node* &head, int data){
+    Node* newNode = new Node(data);
+
+    Node* temp = head;
+
+    while (temp->next !=NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    
+}
+
+
+void insertAtPosition(Node* &head, int data, int position){
+    Node* newNode = new Node(data);
+
+    Node* startNode = head;
+    Node* nextNode = startNode->next;
+
+    while(position>0){
+        startNode = startNode->next;
+        nextNode = startNode->next;
+        
+        position--;
+    }
+    newNode->next = nextNode;
+    startNode->next = newNode;
+}
+
 int main()
 {
     Node* node = new Node(20);
@@ -48,6 +80,21 @@ int main()
     insertAtHead(node, 40);
     insertAtHead(node, 50);
     print(node);
+    cout<<endl;
+
+    insertAtEnd(node,30);
+    insertAtEnd(node,40);
+    insertAtEnd(node,50);
+    print(node);
+    cout<<endl;
+
+
+    insertAtPosition(node, 100,3);
+    insertAtPosition(node, 200,4);
+    insertAtPosition(node, 300,5);
+    print(node);
+
+    
     
     return 0;
 
