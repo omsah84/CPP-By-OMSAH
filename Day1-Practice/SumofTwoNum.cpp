b5b0f99abcf1fa0955fd872a8be0c21b22a1de13
OM PRAKASH SAH <105604367+omsah84@users.noreply.github.com>
2023-11-08 04:14:49 -0500

    Update SumofTwoNum.cpp

--- Day1-Practice/SumofTwoNum.cpp
+++ Day1-Practice/SumofTwoNum.cpp
@@ -8,12 +8,12 @@ int sum(int a, int b){
 int main(){
     int num1, num2;
 
-    //cout << "Enter the two number: ";
+    cout << "Enter the two number: ";
     cin >> num1 >> num2;
 
-    //int sum2num = sum(num1, num2);
+    int sum2num = sum(num1, num2);
 
-    //cout << "The sum of " << num1 << " and " << num2 << " is " << sum2num;
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum2num;
 
     return 0;
 }
